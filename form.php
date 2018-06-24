eb9f0b251b055fdf6e42ddbcdbde5c3a577f7c00
shimoyashikikeisuke <39943876+shimoyashikikeisuke@users.noreply.github.com>
2018-06-24 21:29:05 +0900

    修正１回目です

--- form.php
+++ form.php
@@ -7,7 +7,7 @@
 <body>
     <h1>投稿フォーム</h1>
   <form method = "POST" action = "record.php">
-  <p>
+  <div>
     ニックネーム:<br />
     <input type="text" name="name"size="20"maxlength="15"><br><br>
   	筋トレレベル:<br />
@@ -17,7 +17,7 @@
       <option value="2">中級</option>
       <option value="3">上級</option>
     </select>
-  </p><p>
+  </div><div>
   	ジャンル:<br />
     <select name="muscle_genre">
       <option value="">選択してください</option>
@@ -26,20 +26,22 @@
       <option value="3">その他</option>
     </select>
 
-  </p><p>
+  </div><div>
   	メッセージ:<br />
   	<textarea name="message" cols="50" rows="5"></textarea>
-  </p><p>
+  </div><div>
   	<input type = "submit" value = "登録" />
     </form>
   </body>
 </html>
 
   <?php
-    try {
+    try { //PDOを用いてデータベースに接続
       $pdo = new PDO('mysql:host=localhost;dbname=g031o082;charset=utf8','test','enzyl68x',
       array(PDO::ATTR_EMULATE_PREPARES => false));
     } catch (PDOException $e) {
       exit('データベース接続失敗。'.$e->getMessage());
     }
+
+    
   ?>
