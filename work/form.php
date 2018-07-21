28a2ea241bbe8735d2490f9ae1a70f782bdd7556
下屋敷敬祐 <g031o082@s.iwate-pu.ac.jp>
2018-07-22 08:24:14 +0900

    掲示板 投稿フォーム

--- work/form.php
+++ work/form.php
@@ -1,12 +1,46 @@
  <?php
- try { //PDOを用いてデータベースに接続
-  $pdo = new PDO('mysql:host=localhost;dbname=g031o082;charset=utf8','test','enzyl68x',
-  array(PDO::ATTR_EMULATE_PREPARES => false));
 
-    $dbh = null;
-  } catch (PDOException $e) {
-    exit('データベース接続失敗。'.$e->getMessage());
+  session_start();//セッション開始
+
+  $db_user = 'test';     // ユーザー名
+  $db_pass = 'enzyl68x'; // パスワード
+  $db_name = 'g031o082';     // データベース名
+  // MySQLに接続
+  $mysqli = new mysqli('localhost', $db_user, $db_pass, $db_name);
+  //データベース接続におけるエラー処理
+  if ($mysqli->connect_errno) {
+    printf("%s\n", $mysqli->connect_errno);
+    exit();
   }
+
+     if (isset($_POST["register"])) { //登録ボタンを押されたときの処理
+       if (empty($_POST["name"])) {
+         echo '<script>alert("名前を入力してください");</script>';
+       } else if (empty($_POST["muscle_level"]) && empty($_POST["muscle_genre"])) {
+         echo '<script>alert("レベルまたはジャンルを入力してください");</script>';
+       } else if (empty($_POST["messages"])) {
+         echo '<script>alert("投稿文を入力してください");</script>';
+       } else if (empty($_POST["password"])){
+         '<script>alert("削除・変更用パスワードを入力してください");</script>';
+       }
+
+          //各項目が入力されている場合
+         if ((!empty($_POST['name']) && !empty($_POST['muscle_level'])) && (!empty($_POST['muscle_genre']) && !empty($_POST['messages']) && !empty($_POST['password']))) {
+              //XSSの対策
+             $name = $mysqli->real_escape_string($_POST['name']);
+             $muscle_level = $mysqli->real_escape_string($_POST['muscle_level']);
+             $muscle_genre = $mysqli->real_escape_string($_POST['muscle_genre']);
+             $messages = $mysqli->real_escape_string($_POST['messages']);
+             $password = $mysqli->real_escape_string ($_POST['password']); //削除・変更するときに使用するパスワード
+             //INSERTでデータをデータベースに挿入
+             $insert = $mysqli->query("INSERT INTO `message` ( `name`, `muscle_level`, `muscle_genre`, `messages`, `password`,`created`) VALUES ('{$name}', '{$muscle_level}', '{$muscle_genre}', '{$messages}', '{$password}', now())");
+             if (!$insert) { // insert文におけるエラー処理
+               printf("%s\n", $mysqli->error);
+               exit();
+             }
+             echo '<script> alert("登録が完了しました。"); location.href="muscle_bbd.php"; </script>';
+          }
+     }
 
  ?>
 
@@ -18,7 +52,7 @@
 </head>
 <body>
     <h1>筋肉掲示板</h1>
-  <form method = "POST" action = "muscle_bbd.php">
+  <form method = "POST" action = "">
   <div>
     ニックネーム:<br />
     <input type="text" name="name" size="20"maxlength="15"><br><br>
@@ -42,9 +76,14 @@
   	メッセージ:<br />
   	<textarea name="messages" cols="50" rows="5"></textarea>
   </div><div>
-  	<input type = "submit" value = "登録" />
+    削除・変更用パスワード:<br />
+      <input type="password"maxlength="4" name="password" value="" placeholder="数字４文字">
+  </div><div>
+
+  	<input type = "submit" name = "register"value = "登録" />
+    <ul>
+      <li><a href="muscle_bbd.php">一覧表示</a></li>
+    </ul>
     </form>
-
-
  </body>
 </html>
